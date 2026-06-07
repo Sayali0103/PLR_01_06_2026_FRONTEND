04989656abb2898190a29a0406162f6e33d9ec70
ruchikadingria <ruchikadingria72485@gmail.com>
2026-06-07 11:47:48 +0530

    Allow Railway frontend hostname

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['plr01062026frontend-production.up.railway.app'],
+  },
 })
